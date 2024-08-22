f433fcb9b280275e23aaba59f3d7f617112a1de3
AshokAshok24 <ashok246445@gmail.com>
2024-08-22 23:20:50 +0530

    confirm Popup Model Design

--- commonComponent/ConfirmPopup.jsx
+++ commonComponent/ConfirmPopup.jsx
@@ -1,17 +1,35 @@
+import { confirmVisibility } from '@/lib/slicers/popupSlicer'
 import React from 'react'
 import { Modal } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-const ConfirmPopup = ({ visible }) => {
 
+const ConfirmPopup = ({ visible, setShow }) => {
+
+    const dispatch = useDispatch()
+
+    const handleOnHide = () => { dispatch(confirmVisibility({ show: false })) }
 
     const confirm = useSelector(state => state.popupReducer.confirmPopup)
 
     return (
         <>
-            <Modal show={confirm.show} centered >
+            <Modal show={confirm.show} centered onEscapeKeyDown={handleOnHide} className='popup'>
                 <Modal.Body>
-                    <div>Popup Model Box</div>
+                    <div className="wrapper">
+
+                        <div className="d-flex flex-column align-items-center">
+                            <i class="fa-solid fa-triangle-exclamation"></i>
+                            <h3>Are You Sure ?</h3>
+                            <p>Please Confirm to Proceed Further</p>
+                        </div>
+
+                        <div className='btn-wrapper'>
+                            <button className='cancel'>Cancel</button>
+                            <button className='confirm'>Confirm</button>
+                        </div>
+                    </div>
+
                 </Modal.Body>
             </Modal>
         </>
