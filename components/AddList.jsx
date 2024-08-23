0d75fb586f2411fd547d3b05942698fb3ad6cd00
AshokAshok24 <ashok246445@gmail.com>
2024-08-23 10:35:05 +0530

    Add List to redex state

--- components/AddList.jsx
+++ components/AddList.jsx
@@ -1,16 +1,41 @@
+import { addListItems } from '@/lib/slicers/popupSlicer'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
 const AddList = () => {
+
+    const [inputVal, setInputVal] = React.useState("")
+
+    const dispatch = useDispatch()
+
+    const handleSubmit = () => {
+
+        if (inputVal) {
+
+            dispatch(addListItems(inputVal));
+            setInputVal("")
+        }
+    }
+
     return (
         <>
             <div className='container m-5'>
 
                 <h5>AddList</h5>
 
-                <form>
 
-                    <input type='text' />
-                </form>
+
+                <div className="row">
+                    <div className="col-6">
+                        <input className='form-control' value={inputVal} onChange={(e) => { setInputVal(e.target.value) }} type='text' />
+                        <div className="mt-3">
+                            <button type="submit" onClick={handleSubmit} className='btn btn-success'>Add</button>
+                        </div>
+
+                    </div>
+                </div>
+
+
 
             </div>
         </>
