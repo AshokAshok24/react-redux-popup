080dbc2b67b7a3a6f7396140fad9c75a90e43b01
AshokAshok24 <ashok246445@gmail.com>
2024-08-23 12:57:17 +0530

    ViewList delete Function Added

--- components/ViewList.jsx
+++ components/ViewList.jsx
@@ -1,16 +1,26 @@
+import { confirmVisibility, deleteVisibility } from '@/lib/slicers/popupSlicer';
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const ViewList = () => {
 
+    const dispatch = useDispatch();
+
     const listItems = useSelector(state => state.popupReducer.data)
+
+    const confirmSelector = useSelector(state => state.popupReducer.confirmPopup);
+
+    const deleteSelector = useSelector(state => state.popupReducer.deletePopup)
+
+    const handletDelete = (index) => {
+
+        dispatch(deleteVisibility({ status: false, show: true, id: index }))
+    }
     return (
         <>
             <div className='container m-5'>
 
                 <h5>View List</h5>
-
-
 
                 <div className="row">
                     <div className="col-6">
@@ -32,7 +42,7 @@ const ViewList = () => {
                                         <tr>
                                             <th scope="row">{index + 1}</th>
                                             <td>{item}</td>
-                                            <td className='text-danger'>
+                                            <td className='text-danger' onClick={() => { handletDelete(index) }}>
                                                 <i class="fa-solid fa-trash-can"></i>
                                             </td>
                                         </tr>
