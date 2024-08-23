ceda4303a2edba7b54bd12ee3e7856fbeff5230c
AshokAshok24 <ashok246445@gmail.com>
2024-08-23 08:45:44 +0530

    Tabs components added

---
+++ components/AddList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AddList = () => {
+    return (
+        <>
+            <div className='container m-5'>
+
+                <h5>AddList</h5>
+
+                <form>
+
+                    <input type='text' />
+                </form>
+
+            </div>
+        </>
+    )
+}
+
+export default AddList
---
+++ components/DeleteList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DeleteList = () => {
+    return (
+        <>
+            <div className='container m-5'>
+
+                <div>Delete List Items</div>
+
+            </div>
+        </>
+    )
+}
+
+export default DeleteList
