196aa54c73b7e892c49519e61613fd9e61d54870
AshokAshok24 <ashok246445@gmail.com>
2024-08-23 10:35:30 +0530

    View List show form redux

--- components/ViewList.jsx
+++ components/ViewList.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ViewList = () => {
+
+    const listItems = useSelector(state => state.popupReducer.data)
     return (
         <>
             <div className='container m-5'>
 
-                <div>View List Items</div>
+                <h5>View List</h5>
+
+
+
+                <div className="row">
+                    <div className="col-6">
+
+                        {JSON.stringify(listItems)}
+
+                        <ul className='nav-list'>
+
+
+
+                            {listItems && listItems.map((item, index) => (
+
+                                <>
+
+                                    <li className='list' key={index}>{item}</li>
+                                </>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+
 
             </div>
 
